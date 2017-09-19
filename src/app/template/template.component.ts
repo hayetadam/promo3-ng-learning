ace4828dd1c77f801a7202885ac065e7262bb546
hayet <habethayet@gmail.com>
2017-09-19 14:49:40 +0200

    update

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,20 +1,35 @@
-import{Component} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
+    /*
+    Le nom de la balise html qui sera liée 
+    à ce component
+    */
     selector: 'app-template',
+    /*
+    Le chemin vers le fichier html que gère ce component
+    */
     templateUrl:'./template.component.html'
 })
-
-export class TemplateComponent{
+export class TemplateComponent {
     title = 'app';
-    methode(){
-      this.title ='autre chose';
-    }
+    inputShown:boolean = false;
     personne = {
-        nom:"",
-        prenom:"",
-        age:0
+        nom: '',
+        prenom: '',
+        age:0,
+        chien: {}
+    };
+
+    afficher() {
+        console.log(this.personne);
+    }
+  
+    methode() {
+      this.title = 'autre chose';
+    }
+  
+    toggleInput() {
+      this.inputShown = !this.inputShown;
     }
 }
-
-    
